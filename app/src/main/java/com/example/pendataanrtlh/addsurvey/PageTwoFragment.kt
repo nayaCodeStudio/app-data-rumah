46b781f49385d493e179e559f9aa9bc9b04a6c70
joe_fachrizal <joefachrizal@gmail.com>
2020-09-07 09:23:02 +0700

    penambahan data pada halaman dua

--- app/src/main/java/com/example/pendataanrtlh/addsurvey/PageTwoFragment.kt
+++ app/src/main/java/com/example/pendataanrtlh/addsurvey/PageTwoFragment.kt
@@ -9,16 +9,12 @@ import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.pendataanrtlh.R
 import com.example.pendataanrtlh.databinding.FragmentPageTwoBinding
-import com.example.pendataanrtlh.model.AspekKeselamatan
-import com.example.pendataanrtlh.model.AspekLuasRuang
 import com.example.pendataanrtlh.model.IdentitasPenghuniRmh
 import com.example.pendataanrtlh.utils.Data.IDENTITAS_PENGHUNI_RMH
 import com.example.pendataanrtlh.utils.Data.TEMP_FORM
 import com.example.pendataanrtlh.utils.Data.nikPeserta
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.fragment_page_three.*
-import kotlinx.android.synthetic.main.fragment_page_two.*
 
 
 class PageTwoFragment : Fragment() {
@@ -27,7 +23,14 @@ class PageTwoFragment : Fragment() {
     private lateinit var myRef: DatabaseReference
 
     private var jenisKelamin: String? = ""
-    private var pendidikanTrakhir: String? = ""
+    private var pendidikan: String? = ""
+    private var pekerjaan: String? = ""
+    private var penghasilan: String? = ""
+    private var statusTanah: String? = ""
+    private var statusRumah: String? = ""
+    private var assetRumah: String? = ""
+    private var assetTanah: String? = ""
+    private var bantuanRumah: String? = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,6 +53,9 @@ class PageTwoFragment : Fragment() {
             val inNomorRumah = binding.textNoRumah.text.toString().trim { it <= ' ' }
             val inNamaLengkap = binding.textNmLengkap.text.toString().trim { it <= ' ' }
             val inUsia = binding.textUsia.text.toString().trim { it <= ' ' }
+            val inAlmLengkp = binding.textAlmtLengkap.text.toString().trim { it <= ' ' }
+            val inNoKtp = binding.textNoKTP.text.toString().trim { it <= ' ' }
+            val inJumlhKK = binding.textJumKK.text.toString().trim { it <= ' ' }
             var inputKosong = false
 
             when {
@@ -65,44 +71,71 @@ class PageTwoFragment : Fragment() {
                     inputKosong = true
                     binding.textUsia.error = getString(R.string.msg_error_empty)
                 }
+                inAlmLengkp.isEmpty() -> {
+                    inputKosong = true
+                    binding.textAlmtLengkap.error = getString(R.string.msg_error_empty)
+                }
+                inNoKtp.isEmpty() -> {
+                    inputKosong = true
+                    binding.textNoKTP.error = getString(R.string.msg_error_empty)
+                }
+                inJumlhKK.isEmpty() -> {
+                    inputKosong = true
+                    binding.textJumKK.error = getString(R.string.msg_error_empty)
+                }
             }
 
-            if (!inputKosong) {
+            pendidikan = binding.listPendidikan.selectedItem.toString()
+            pekerjaan = binding.listPekerjaan.selectedItem.toString()
+            penghasilan = binding.listPenghasilan.selectedItem.toString()
+            statusTanah = binding.listStatusTanah.selectedItem.toString()
+            statusRumah = binding.listStatusRumah.selectedItem.toString()
+            bantuanRumah = binding.listBantuanPerumahan.selectedItem.toString()
 
-            }
-
-            pendidikanTrakhir = binding.listPendidikan.selectedItem.toString()
-
-            jenisKelamin = if (chip_pria.isChecked) {
+            jenisKelamin = if (binding.chipPria.isChecked) {
                 "Pria"
             } else {
                 "Wanita"
             }
 
-            if (!jenisKelamin.isNullOrEmpty() && pendidikanTrakhir != "pilih") {
-                myRef.setValue(
-                    IdentitasPenghuniRmh(
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        "",
-                        ""
+            assetRumah = if (binding.rbAdaRumah.isChecked) {
+                "Ada"
+            } else {
+                "Tidak Ada"
+            }
+
+            assetTanah = if (binding.rbAdaTanah.isChecked) {
+                "Ada"
+            } else {
+                "Tidak Ada"
+            }
+
+            if (!inputKosong) {
+                if (!jenisKelamin.isNullOrEmpty() && pendidikan != "pilih") {
+                    myRef.setValue(
+                        IdentitasPenghuniRmh(
+                            inNomorRumah,
+                            inNamaLengkap,
+                            "",
+                            pendidikan,
+                            jenisKelamin,
+                            inAlmLengkp,
+                            inNoKtp,
+                            inJumlhKK,
+                            pekerjaan,
+                            penghasilan,
+                            statusTanah,
+                            statusRumah,
+                            assetRumah,
+                            assetTanah,
+                            bantuanRumah,
+                            ""
+                        )
                     )
-                )
-                    .addOnCompleteListener {
-                        findNavController().navigate(R.id.action_SecondFragment_to_ThirdFragment)
-                    }
+                        .addOnCompleteListener {
+                            findNavController().navigate(R.id.action_SecondFragment_to_ThirdFragment)
+                        }
+                }
             } else {
                 Toast.makeText(context, "Harap Diisi dahulu", Toast.LENGTH_SHORT).show()
             }
