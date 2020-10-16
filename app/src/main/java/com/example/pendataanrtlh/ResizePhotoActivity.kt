2199abd6db24dc5158f224cfddfbbb18410b8330
joe_fachrizal <joefachrizal@gmail.com>
2020-10-16 16:42:57 +0700

    menambah koding pada halaman 3 untuk meng upload gambar

--- app/src/main/java/com/example/pendataanrtlh/ResizePhotoActivity.kt
+++ app/src/main/java/com/example/pendataanrtlh/ResizePhotoActivity.kt
@@ -27,96 +27,22 @@ class ResizePhotoActivity : AppCompatActivity() {
         private const val PICK_IMAGE_REQUEST_2 = 3
     }
 
-    private var actualImage: File? = null
-    private var compressedImage: File? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resize_photo)
-        actualImageView.setBackgroundColor(getRandomColor())
-        clearImage()
         setupClickListener()
     }
 
     private fun setupClickListener() {
-        chooseImageButton.setOnClickListener { chooseImage() }
-        compressImageButton.setOnClickListener {
-            for (i in 0 until 3) {
-                Toast.makeText(this, i, Toast.LENGTH_SHORT).show()
-            }
-        }
         fotoKondisi1.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST) }
         fotoKondisi2.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_1) }
         fotoKondisi3.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_2) }
-//        customCompressImageButton.setOnClickListener { customCompressImage() }
-    }
-
-    private fun chooseImage() {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "image/*"
-        startActivityForResult(intent, PICK_IMAGE_REQUEST)
     }
 
     private fun chooseImageMulti(requestCode: Int) {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         startActivityForResult(intent, requestCode)
-    }
-
-/*    private fun compressImage() {
-        actualImage?.let { imageFile ->
-            lifecycleScope.launch {
-                // Default compression
-                compressedImage = Compressor.compress(this@ResizePhotoActivity, imageFile)
-                val filee = Uri.fromFile(compressedImage)
-//                uploadImageDua(filee)
-//                setCompressedImage()
-                imgPath = filee
-//                finish()
-            }
-        } ?: showError("Please choose an image!")
-    }*/
-
-//    private fun customCompressImage() {
-//        actualImage?.let { imageFile ->
-//            lifecycleScope.launch {
-//                // Default compression with custom destination file
-//                compressedImage = Compressor.compress(this@ResizePhotoActivity, imageFile) {
-//                    default()
-//                    getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.also {
-//                        val file = File("${it.absolutePath}${File.separator}my_image.${imageFile.extension}")
-//                        destination(file)
-//                        val filee = Uri.fromFile(file)
-//                        uploadImageDua(filee)
-//                    }
-//                }
-//
-//                // Full custom
-///*                compressedImage = Compressor.compress(this@ResizePhotoActivity, imageFile) {
-//                    resolution(1280, 720)
-//                    quality(80)
-//                    format(Bitmap.CompressFormat.WEBP)
-//                    size(2_097_152) // 2 MB
-//                }*/
-//                setCompressedImage()
-//            }
-//        } ?: showError("Please choose an image!")
-//    }
-
-/*    private fun setCompressedImage() {
-        compressedImage?.let {
-            compressedImageView.setImageBitmap(BitmapFactory.decodeFile(it.absolutePath))
-            compressedSizeTextView.text = String.format("Size : %s", getReadableFileSize(it.length()))
-            Toast.makeText(this, "Compressed image save in " + it.path, Toast.LENGTH_LONG).show()
-            Log.d("Compressor", "Compressed image save in " + it.path)
-        }
-    }*/
-
-    private fun clearImage() {
-        actualImageView.setBackgroundColor(getRandomColor())
-        compressedImageView.setImageDrawable(null)
-        compressedImageView.setBackgroundColor(getRandomColor())
-        compressedSizeTextView.text = "Size : -"
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -140,7 +66,6 @@ class ResizePhotoActivity : AppCompatActivity() {
 //                                img1 = filee
                                 uploadImageDua(filee, "foto1")
                                 fotoKondisi1.setImageBitmap(loadBitmap(compressedImage))
-                                clearImage()
                             }
                         }
                     } catch (e: IOException) {
@@ -167,7 +92,6 @@ class ResizePhotoActivity : AppCompatActivity() {
 //                                img2 = filee
                                 uploadImageDua(filee, "foto2")
                                 fotoKondisi2.setImageBitmap(loadBitmap(compressedImage))
-                                clearImage()
                             }
                         }
                     } catch (e: IOException) {
@@ -194,7 +118,6 @@ class ResizePhotoActivity : AppCompatActivity() {
 //                                img3 = filee
                                 uploadImageDua(filee, "foto3")
                                 fotoKondisi3.setImageBitmap(loadBitmap(compressedImage))
-                                clearImage()
                             }
                         }
                     } catch (e: IOException) {
@@ -204,54 +127,11 @@ class ResizePhotoActivity : AppCompatActivity() {
                 }
             }
         }
-/*        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
-            if (data == null) {
-                showError("Failed to open picture!")
-                return
-            }
-            try {
-                actualImage = FileUtil.from(this, data.data)?.also { imageFile ->
-
-                    lifecycleScope.launch {
-                        // Default compression
-                        val compressedImage = Compressor.compress(this@ResizePhotoActivity, imageFile)
-//                            val filee = Uri.fromFile(compressedImage)
-//                            uploadImageDua(filee)
-//                            imgPath = filee
-                        actualImageView.setImageBitmap(loadBitmap(compressedImage))
-                        actualSizeTextView.text = String.format("Size : %s", getReadableFileSize(compressedImage.length()))
-                        clearImage()
-                    }
-
-//                    actualImageView.setImageBitmap(loadBitmap(imageFile))
-//                    actualSizeTextView.text = String.format("Size : %s", getReadableFileSize(imageFile.length()))
-//                    clearImage()
-                }
-            } catch (e: IOException) {
-                showError("Failed to read picture data!")
-                e.printStackTrace()
-            }
-        }*/
     }
 
     private fun showError(errorMessage: String) {
         Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
     }
-
-    private fun getRandomColor() = Random().run {
-        Color.argb(100, nextInt(256), nextInt(256), nextInt(256))
-    }
-
-/*
-    private fun getReadableFileSize(size: Long): String {
-        if (size <= 0) {
-            return "0"
-        }
-        val units = arrayOf("B", "KB", "MB", "GB", "TB")
-        val digitGroups = (log10(size.toDouble()) / log10(1024.0)).toInt()
-        return DecimalFormat("#,##0.#").format(size / 1024.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
-    }
-*/
 
     private fun uploadImageDua(file: Uri, nameImage: String) {
         val pathImage = "gambar/$nameImage.jpg"
@@ -282,6 +162,5 @@ class ResizePhotoActivity : AppCompatActivity() {
                 progressBar.progress = progress.toInt()
             }
     }
-
 }
 
--- app/src/main/java/com/example/pendataanrtlh/addsurvey/PageThreeFragment.kt
+++ app/src/main/java/com/example/pendataanrtlh/addsurvey/PageThreeFragment.kt
@@ -1,22 +1,42 @@
 package com.example.pendataanrtlh.addsurvey
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.pendataanrtlh.R
 import com.example.pendataanrtlh.databinding.FragmentPageThreeBinding
 import com.example.pendataanrtlh.utils.Data.atap
 import com.example.pendataanrtlh.utils.Data.balok
+import com.example.pendataanrtlh.utils.Data.noKTPUser
 import com.example.pendataanrtlh.utils.Data.pondasi
-import kotlinx.android.synthetic.main.fragment_page_three.*
+import com.example.pendataanrtlh.utils.FileUtil
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import id.zelory.compressor.Compressor
+import id.zelory.compressor.loadBitmap
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 
 class PageThreeFragment : Fragment() {
     private lateinit var binding: FragmentPageThreeBinding
+
+    companion object {
+        private const val PICK_IMAGE_REQUEST_1 = 1
+        private const val PICK_IMAGE_REQUEST_2 = 2
+        private const val PICK_IMAGE_REQUEST_3 = 3
+        private const val PICK_IMAGE_REQUEST_4 = 4
+        private const val PICK_IMAGE_REQUEST_5 = 5
+        private const val PICK_IMAGE_REQUEST_6 = 6
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,7 +57,7 @@ class PageThreeFragment : Fragment() {
         binding.btnNext.setOnClickListener {
 //            findNavController().navigate(R.id.action_ThirdFragment_to_FourFragment)
 
-            val inPondasi = if (chip_ada.isChecked) {
+            val inPondasi = if (binding.chipAda.isChecked) {
                 "Ada"
             } else {
                 "Tidak Ada"
@@ -47,7 +67,8 @@ class PageThreeFragment : Fragment() {
 
             if (!inPondasi.isEmpty() &&
                 inBalok != "pilih" &&
-                inAtap != "pilih") {
+                inAtap != "pilih"
+            ) {
                 //One Push Methode
                 pondasi = inPondasi
                 balok = inBalok
@@ -58,5 +79,211 @@ class PageThreeFragment : Fragment() {
                 Toast.makeText(context, "Harap diisi dahulu!", Toast.LENGTH_SHORT).show()
             }
         }
+
+        setupClickListener()
+    }
+
+    private fun setupClickListener() {
+        binding.fotoKondisiKolom1.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_1) }
+        binding.fotoKondisiKolom2.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_2) }
+        binding.fotoKondisiKolom3.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_3) }
+        binding.fotoKondisiKonstruksiAtap1.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_4) }
+        binding.fotoKondisiKonstruksiAtap2.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_5) }
+        binding.fotoKondisiKonstruksiAtap3.setOnClickListener { chooseImageMulti(PICK_IMAGE_REQUEST_6) }
+    }
+
+    private fun chooseImageMulti(requestCode: Int) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, requestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PICK_IMAGE_REQUEST_1 -> {
+                if (requestCode == PICK_IMAGE_REQUEST_1 && resultCode == RESULT_OK) {
+                    if (data == null) {
+                        showError("Failed to open picture!")
+                        return
+                    }
+                    try {
+                        FileUtil.from(requireContext(), data.data)?.also { imageFile ->
+                            lifecycleScope.launch {
+                                // Default compression
+                                val compressedImage = Compressor.compress(
+                                    requireContext(),
+                                    imageFile
+                                )
+                                val file = Uri.fromFile(compressedImage)
+                                uploadImageDua(file, "form3_${requestCode}_$noKTPUser")
+                                binding.fotoKondisiKolom1.setImageBitmap(loadBitmap(compressedImage))
+                            }
+                        }
+                    } catch (e: IOException) {
+                        showError("Failed to read picture data!")
+                        e.printStackTrace()
+                    }
+                }
+            }
+            PICK_IMAGE_REQUEST_2 -> {
+                if (requestCode == PICK_IMAGE_REQUEST_2 && resultCode == RESULT_OK) {
+                    if (data == null) {
+                        showError("Failed to open picture!")
+                        return
+                    }
+                    try {
+                        FileUtil.from(requireContext(), data.data)?.also { imageFile ->
+                            lifecycleScope.launch {
+                                // Default compression
+                                val compressedImage = Compressor.compress(
+                                    requireContext(),
+                                    imageFile
+                                )
+                                val file = Uri.fromFile(compressedImage)
+                                uploadImageDua(file, "form3_${requestCode}_$noKTPUser")
+                                binding.fotoKondisiKolom2.setImageBitmap(loadBitmap(compressedImage))
+                            }
+                        }
+                    } catch (e: IOException) {
+                        showError("Failed to read picture data!")
+                        e.printStackTrace()
+                    }
+                }
+            }
+            PICK_IMAGE_REQUEST_3 -> {
+                if (requestCode == PICK_IMAGE_REQUEST_3 && resultCode == RESULT_OK) {
+                    if (data == null) {
+                        showError("Failed to open picture!")
+                        return
+                    }
+                    try {
+                        FileUtil.from(requireContext(), data.data)?.also { imageFile ->
+                            lifecycleScope.launch {
+                                // Default compression
+                                val compressedImage = Compressor.compress(
+                                    requireContext(),
+                                    imageFile
+                                )
+                                val file = Uri.fromFile(compressedImage)
+                                uploadImageDua(file, "form3_${requestCode}_$noKTPUser")
+                                binding.fotoKondisiKolom3.setImageBitmap(loadBitmap(compressedImage))
+                            }
+                        }
+                    } catch (e: IOException) {
+                        showError("Failed to read picture data!")
+                        e.printStackTrace()
+                    }
+                }
+            }
+            PICK_IMAGE_REQUEST_4 -> {
+                if (requestCode == PICK_IMAGE_REQUEST_4 && resultCode == RESULT_OK) {
+                    if (data == null) {
+                        showError("Failed to open picture!")
+                        return
+                    }
+                    try {
+                        FileUtil.from(requireContext(), data.data)?.also { imageFile ->
+                            lifecycleScope.launch {
+                                // Default compression
+                                val compressedImage = Compressor.compress(
+                                    requireContext(),
+                                    imageFile
+                                )
+                                val file = Uri.fromFile(compressedImage)
+                                uploadImageDua(file, "form3_${requestCode}_$noKTPUser")
+                                binding.fotoKondisiKonstruksiAtap1.setImageBitmap(loadBitmap(compressedImage))
+                            }
+                        }
+                    } catch (e: IOException) {
+                        showError("Failed to read picture data!")
+                        e.printStackTrace()
+                    }
+                }
+            }
+            PICK_IMAGE_REQUEST_5 -> {
+                if (requestCode == PICK_IMAGE_REQUEST_5 && resultCode == RESULT_OK) {
+                    if (data == null) {
+                        showError("Failed to open picture!")
+                        return
+                    }
+                    try {
+                        FileUtil.from(requireContext(), data.data)?.also { imageFile ->
+                            lifecycleScope.launch {
+                                // Default compression
+                                val compressedImage = Compressor.compress(
+                                    requireContext(),
+                                    imageFile
+                                )
+                                val file = Uri.fromFile(compressedImage)
+                                uploadImageDua(file, "form3_${requestCode}_$noKTPUser")
+                                binding.fotoKondisiKonstruksiAtap2.setImageBitmap(loadBitmap(compressedImage))
+                            }
+                        }
+                    } catch (e: IOException) {
+                        showError("Failed to read picture data!")
+                        e.printStackTrace()
+                    }
+                }
+            }
+            PICK_IMAGE_REQUEST_6 -> {
+                if (requestCode == PICK_IMAGE_REQUEST_6 && resultCode == RESULT_OK) {
+                    if (data == null) {
+                        showError("Failed to open picture!")
+                        return
+                    }
+                    try {
+                        FileUtil.from(requireContext(), data.data)?.also { imageFile ->
+                            lifecycleScope.launch {
+                                // Default compression
+                                val compressedImage = Compressor.compress(
+                                    requireContext(),
+                                    imageFile
+                                )
+                                val file = Uri.fromFile(compressedImage)
+                                uploadImageDua(file, "form3_${requestCode}_$noKTPUser")
+                                binding.fotoKondisiKonstruksiAtap3.setImageBitmap(loadBitmap(compressedImage))
+                            }
+                        }
+                    } catch (e: IOException) {
+                        showError("Failed to read picture data!")
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun uploadImageDua(file: Uri, nameImage: String) {
+        val pathImage = "gambar/$nameImage.jpg"
+        val storageRef = FirebaseStorage
+            .getInstance()
+            .reference.child(pathImage)
+
+        val databaseKonf = FirebaseDatabase
+            .getInstance()
+            .getReference("uploadFoto")
+            .child(noKTPUser.toString())
+
+        storageRef.putFile(file)
+            .addOnSuccessListener {
+                storageRef.downloadUrl.addOnSuccessListener {
+                    databaseKonf.child(nameImage).setValue(it.toString())
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Add Image Successfully", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener {
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(requireContext(), "Info File : ${it.message}", Toast.LENGTH_SHORT).show()
+            }.addOnProgressListener { taskSnapshot ->
+                binding.progressBar.visibility = View.VISIBLE
+                val progress = 100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount
+                binding.progressBar.progress = progress.toInt()
+            }
+    }
+
+    private fun showError(errorMessage: String) {
+        Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
     }
 }
