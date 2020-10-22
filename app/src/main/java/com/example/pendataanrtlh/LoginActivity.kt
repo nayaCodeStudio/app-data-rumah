7c43e0eba4b0eb45903042f6d8bbe47b33203145
joe_fachrizal <joefachrizal@gmail.com>
2020-10-23 05:20:57 +0700

    membuat blokir pada login

--- app/src/main/java/com/example/pendataanrtlh/LoginActivity.kt
+++ app/src/main/java/com/example/pendataanrtlh/LoginActivity.kt
@@ -1,14 +1,19 @@
 package com.example.pendataanrtlh
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.pendataanrtlh.databinding.ActivityLoginBinding
 import com.example.pendataanrtlh.model.RegisterForm
+import com.example.pendataanrtlh.model.Status
 import com.example.pendataanrtlh.utils.Data
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.database.*
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -17,7 +22,12 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var myRef1: DatabaseReference
     private lateinit var inUser: String
     private lateinit var inPassword: String
-    private var versi: String? = "beta"
+    private lateinit var countDownTimer: CountDownTimer
+
+    //    private var timeValue: Long = 1
+    private var beta: String? = "beta"
+    private var trial: String? = "trial"
+    private var full: String? = "full"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +36,6 @@ class LoginActivity : AppCompatActivity() {
         database = FirebaseDatabase.getInstance()
         myRef = database.getReference(Data.REGISTER_FORM)
 
-        /*       binding.logoApp.setOnClickListener {
-                   val intentLoginActivity =
-                       Intent(Intent(this, ResizePhotoActivity::class.java))
-                   startActivity(intentLoginActivity)
-               }
-       */
-        binding.textNomorKTP.setText("12345")
-        binding.textPassword.setText("1")
         onLock()
 
 //        binding.btnLogin.setOnLongClickListener {
@@ -48,35 +50,52 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun onLock() {
-        myRef1 = database.getReference("versiKode")
+        myRef1 = database.getReference("status")
         myRef1.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val versionCode = snapshot.getValue(String::class.java)
-                if (versi == versionCode) {
-                    onShowDialog()
-                }
-                binding.btnLogin.setOnClickListener {
-                    if (versi == versionCode) {
-                        onShowDialog()
-                    } else {
-                        inUser = binding.textNomorKTP.text.toString().trim { it <= ' ' }
-                        inPassword = binding.textPassword.text.toString().trim { it <= ' ' }
+                val versionCode = snapshot.getValue(Status::class.java)
 
-                        var inputKosong = false
-                        when {
-                            inUser.isEmpty() -> {
-                                inputKosong = true
-                                binding.textNomorKTP.error = getString(R.string.msg_error_empty)
-                            }
-                            inPassword.isEmpty() -> {
-                                inputKosong = true
-                                binding.textPassword.error = getString(R.string.msg_error_empty)
-                            }
+                when (versionCode?.versiKode) {
+                    beta -> {
+                        onShowDialog("Aplikasi Sedang Ada perbaikan")
+                    }
+                    trial -> {
+                        onShowDialog("Anda Sedang Berada di versi Percobaan")
+                    }
+                    full -> {
+                        onShowDialog("Selamat Datang Di e-Sarulah")
+                    }
+                }
+
+                binding.btnLogin.setOnClickListener {
+                    inUser = binding.textNomorKTP.text.toString().trim { it <= ' ' }
+                    inPassword = binding.textPassword.text.toString().trim { it <= ' ' }
+
+                    var inputKosong = false
+                    when {
+                        inUser.isEmpty() -> {
+                            inputKosong = true
+                            binding.textNomorKTP.error = getString(R.string.msg_error_empty)
                         }
-                        if (!inputKosong) {
-                            onGetData(inUser, inPassword)
+                        inPassword.isEmpty() -> {
+                            inputKosong = true
+                            binding.textPassword.error = getString(R.string.msg_error_empty)
                         }
                     }
+                    if (!inputKosong) {
+                        when (versionCode?.versiKode) {
+                            beta -> {
+                                onShowDialog(versionCode?.pesan.toString())
+                            }
+                            trial -> {
+                                onCountDownTimer(inUser, inPassword)
+                            }
+                            else -> {
+                                onGetData(inUser, inPassword)
+                            }
+                        }
+                    }
+
                 }
             }
 
@@ -116,14 +135,56 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
-    private fun onShowDialog() {
+    private fun onShowDialog(pesan: String) {
         MaterialAlertDialogBuilder(this)
             .setTitle("Peringatan!")
-            .setMessage("Aplikasi Anda tidak bisa diakses")
+            .setMessage(pesan)
 
             .setPositiveButton("Ya") { _, _ ->
-                finish()
+                null
             }
             .show()
+    }
+
+    private fun onCountDownTimer(inUser: String, inPassword: String) {
+        binding.btnLogin.isEnabled = false
+        binding.btnLogin.setBackgroundColor(ContextCompat.getColor(this, R.color.textColor2))
+        var timeValue: Long = 1
+        countDownTimer = object : CountDownTimer(timeValue * 30000, 1000) {
+            @SuppressLint("SetTextI18n")
+            override fun onTick(timeFinished: Long) {
+                timeValue = timeFinished
+
+                val hours = (timeValue / 1000).toInt() / 3600
+                val minutes = (timeValue / 1000 % 3600).toInt() / 60
+                val seconds = (timeValue / 1000).toInt() % 60
+
+                val timeLeftFormatted: String
+                timeLeftFormatted = if (hours > 0) {
+                    String.format(
+                        Locale.getDefault(), "%02d:%02d",  minutes, seconds
+                    )
+                } else {
+                    String.format(
+                        Locale.getDefault(), "%02d", seconds
+                    )
+                }
+                binding.btnLogin.text = "Masuk dalam $timeLeftFormatted detik"
+            }
+
+            override fun onFinish() {
+                val text = "Persiapan..."
+//                binding.btnLogin.isEnabled = true
+                binding.btnLogin.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@LoginActivity,
+                        R.color.bgBiru
+                    )
+                )
+                binding.btnLogin.text = text
+                onGetData(inUser, inPassword)
+            }
+
+        }.start()
     }
 }
---
+++ app/src/main/java/com/example/pendataanrtlh/model/Status.kt
@@ -0,0 +1,9 @@
+package com.example.pendataanrtlh.model
+
+/**
+ ** Written by @JoeFachrizal 23/10/2020 03.32
+ **/
+class Status(
+    var pesan: String? = null,
+    var versiKode: String? = null,
+)
